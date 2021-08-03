e34ded4d0657fefbc91f9e543b64f1f1bcc33660
Jeff TANG <jeff.tang@asmpt.com>
2021-08-03 10:59:44 +0800

    update registry

--- up.bat
+++ up.bat
@@ -1,5 +1,5 @@
-docker build -t angulartest .
+docker build -t 10.18.30.15:5000/angulartest .
 #Remove old container
 docker stop testsite
 docker rm testsite
-docker run -p 8081:80 -d --restart=always --name testsite angulartest
+docker run -p 8081:80 -d --restart=always --name testsite 10.18.30.15:5000/angulartest
